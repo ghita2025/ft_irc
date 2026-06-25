764207516751aa48c5a5c7d172279b89fbfa3d33
ahaded777 <abdoillahahdad0@gmail.com>
2026-06-25 15:56:11 +0100

    Fix topic command numeric replies

--- src/commands/topic.cpp
+++ src/commands/topic.cpp
@@ -10,18 +10,38 @@ void Server::handleTopic(Client& client, Command& cmd)
     std::string channelName = cmd.args[0];
     std::map<std::string, Channel>::iterator it = channels.find(channelName);
     if (it == channels.end())
+    {
+        std::string err = ":" + client.getNickname() + " 403 " + channelName + " :No such channel\r\n";
+        send(client.getFd(), err.c_str(), err.size(), 0);
         return ;
+    }
     Channel &channel = it->second;
     if (!channel.hasMember(client.getFd()))
+    {
+        std::string err = ":" + client.getNickname() + " 442 " + channelName + " :You're not on that channel\r\n";
+        send(client.getFd(), err.c_str(), err.size(), 0);
         return ;
+    }
     if (cmd.args.size() == 1)
     {
-        std::string msg = ":" + channelName + " TOPIC :" + channel.getTopic() + "\r\n";
-        send(client.getFd(), msg.c_str(), msg.size(), 0);
+        if (channel.getTopic().empty())
+        {
+            std::string reply = ":" + client.getNickname() + " 331 " + channelName + " :No topic is set\r\n";
+            send(client.getFd(), reply.c_str(), reply.size(), 0);
+        }
+        else
+        {
+            std::string reply = ":" + client.getNickname() + " 332 " + channelName + " :" + channel.getTopic() + "\r\n";
+            send(client.getFd(), reply.c_str(), reply.size(), 0);
+        }
         return ;
     }
     if (channel.isTopicRestricted() && !channel.isOperator(client.getFd()))
+    {
+        std::string err = ":" + client.getNickname() + " 482 " + channelName + " :You're not channel operator\r\n";
+        send(client.getFd(), err.c_str(), err.size(), 0);
         return ;
+    }
     std::string newTopic = cmd.args[1];
     channel.setTopic(newTopic);
     std::string msg = ":" + client.getNickname() + " TOPIC " + channelName + " :" + newTopic + "\r\n";
